38b7cbe3dd0fecb7d8190e18c7037528d9091def
Michael Holmes <emailmichaelholmes@gmail.com>
2017-04-04 10:44:16 +1000

    Initial Commit

---
+++ pequad-c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pequad-c
+//
+//  Created by Michael Holmes on 4/4/17.
+//  Copyright © 2017 Michael Holmes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
