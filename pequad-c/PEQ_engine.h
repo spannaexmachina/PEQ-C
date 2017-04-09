d8048688e69e531e099b5dc9f41a4f97df1c848e
Michael <email.michaelholmes@gamil.com>
2017-04-09 16:37:11 +1000

    new disclaimers on header files

--- pequad-c/PEQ_engine.h
+++ pequad-c/PEQ_engine.h
@@ -1,10 +1,48 @@
-//
-//  PEQ_engine.h
-//  pequad-c
-//
-//  Created by Michael Holmes on 4/4/17.
-//  Copyright © 2017 Michael Holmes. All rights reserved.
-//
+/************************************************************************************
+ * Copyright (C) 2017  Michael Holmes et al.   <peqclib@gmail.com>                  *
+ *                                                                                  *
+ * This file is part of PEQUOD-C.                                                   *
+ *                                                                                  *
+ *      Redistribution and use in source and binary forms, with or                  *
+ *      without modification, are permitted provided that the                       *
+ *      following conditions are met:                                               *
+ *                                                                                  *
+ *          * Redistributions of source code must retain the above                  *
+ *            copyright notice, this list of conditions and the                     *
+ *            following disclaimer.                                                 *
+ *                                                                                  *
+ *          * Redistributions in binary form must reproduce the                     *
+ *            above copyright notice, this list of conditions and                   *
+ *            the following disclaimer in the documentation and/or                  *
+ *            other materials provided with the distribution.                       *
+ *                                                                                  *
+ *          * Neither the name of PEQUOD-C, PEQ-C, PEQ, nor the                     *
+ *            names of its contributors may be used to endorse or                   *
+ *            promote products derived from this software without                   *
+ *            specific prior written permission.                                    *
+ *                                                                                  *
+ *      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS         *
+ *      “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT           *
+ *      LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS           *
+ *      FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE              *
+ *      COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         *
+ *      INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,        *
+ *      BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;            *
+ *      LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER            *
+ *      CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT          *
+ *      LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY       *
+ *      WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY     *
+ *      OF SUCH DAMAGE.                                                             *
+ *                                                                                  *
+ *      * PEQUOD-C utilises the SDL framework                                       *
+ *        ( https://www.libsdl.org )                                                *
+ *                                                                                  *
+ *      * PEQUOD-C contact:                                                         *
+ *          <peqclib@gmail.com>                                                     *
+ *         ( https://github.com/spannaexmachina/pequad-c )                          *
+ *                                                                                  *
+ ************************************************************************************/
+
 
 #ifndef PEQ_engine_h
 #define PEQ_engine_h
@@ -140,17 +178,29 @@ int PEQ_clean(PEQ_DATA *data);
 int PEQ_cycle(PEQ_DATA *data);
 
 /**
+ * @brief main update loop
+ *
+ * all object updates should be here.
+ * @warning PEQ CORE FUNCTION
+ * @param data PEQ_DATA type pointer
+ * @return 0 success
  */
 int PEQ_update(PEQ_DATA *data);
 
 /**
- * \brief populates a PEQ_DATA struct
- * \param data PEQ_DATA type pointer to populate
- * \returns 0 success;
+ * @brief populates a PEQ_DATA struct
+ * @param data PEQ_DATA type pointer to populate
+ * @warning PEQ CORE FUNCTION
+ * @return 0 success;
  */
 int pop_main_data(PEQ_DATA *data);
 
-
+/**
+ * @brief loads objects into PEQ_data
+ 
+ * @param data PEQ_DATA type pointer to populate
+ * @warning PEQ CORE FUNCTION
+ */
 void load_objects(PEQ_DATA *data);
 
 
