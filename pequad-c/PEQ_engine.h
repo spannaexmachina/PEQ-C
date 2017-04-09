6a3cc9573731e2047335b8b9444c969927cf9eb4
Michael <email.michaelholmes@gamil.com>
2017-04-09 15:50:39 +1000

    DOCUMENTATION updated engine

--- pequad-c/PEQ_engine.h
+++ pequad-c/PEQ_engine.h
@@ -20,7 +20,11 @@
 #include "PEQ_utility.h"
 
 
-
+/**
+ * @brief mask for window modes
+ *
+ * mostly used for passing into low level SDL window functions
+ */
 typedef enum window {
     WINDOW,                     /**< Open in a fixed size window  */
     RESIZABLE,                  /**< Open in a resizable window */
@@ -29,8 +33,30 @@ typedef enum window {
 
 
 
-
+/**
+ * @brief main data struct for PEQUAD-C
+ *
+ * This struct contains all the information PEQUOD needs to run. It
+ * prvides a convinient way to pass data around the program.
+ *
+ * However, custom data does not have to be added here, but can be
+ * used as usual from within new functions.
+ * @code
+ * int PEQ_render(PEQ_DATA *data)
+ * {
+ * ...
+ * PEQ_draw_shape(data->renderer, &data->object_bank[i].graphic.shape);
+ * ...
+ * }
+ * @endcode
+ * @warning Changing the default data struct can have unforseen consiquences!
+ * new fields for cusotmisation should be added at the end and separated from
+ * core data.
+ *
+ */
 typedef struct data {
+    
+    //PEQ default fields
     SDL_Renderer *renderer;     /**< pointer to application renderer */
     SDL_Window *window;         /**< pointer to application window */
     int is_running,             /**< is the application running; 0 false */
@@ -46,53 +72,70 @@ typedef struct data {
     PEQ_COLOUR r_colour;        /**< background render colour (set in precompiler) */
     PEQ_WINDOW_MODE window_mode;/**< window mode (set at precompile) */
     PEQ_OBJECT object_bank[10]; /**< stores objects */
+    //end PEQ default fields
+    
+    /* add custom fields here */
+    
 } PEQ_DATA;
 
 
 
 /**
- * \brief returns an SDL window mode based on PEQ enum
+ * @brief returns an SDL window mode based on PEQ enum
  *
- * \note avaliabe window option are WINDOW; RESIZABLE; FULLSCREEN;
- * \param w_mode PEQ enum window mode
- * \returns SDL window flag
+ * avaliabe window option are WINDOW; RESIZABLE; FULLSCREEN;
+ * @warning PEQ CORE FUNCTION
+ * @param w_mode PEQ enum window mode
+ * @return SDL window flag
  */
 int get_window_mode(PEQ_WINDOW_MODE w_mode);
 
 /**
- * \brief Primary initialising function
- * \par initialises SDL; window; and renderer
- * \param data PEQ_DATA type pointer
- * \returns 1 pass, 0 fail
+ * @brief Primary initialising function
+ *
+ * initialises SDL; window; and renderer
+ * @warning PEQ CORE FUNCTION
+ * @param data PEQ_DATA type pointer
+ * @return 1 pass, 0 fail
  */
 int PEQ_init(PEQ_DATA *data);
 
 /**
- * \brief primary engine loop for handling events
- * \param data PEQ_DATA type pointer
- * \returns 0 success
+ * @brief primary engine loop for handling events
+ *
+ * Any input handing should be put here
+ * @param data PEQ_DATA type pointer
+ * @warning PEQ CORE FUNCTION
+ * @return 0 success
  */
 int PEQ_handle_events(PEQ_DATA *data);
 
 /**
- * \brief primary engine loop for rendering
- * \param data PEQ_DATA type pointer
- * \returns 0 success
+ * @brief primary engine loop for rendering
+ * any rendering tasks should be put here.
+ * @warning PEQ CORE FUNCTION
+ * @param data PEQ_DATA type pointer
+ * @return 0 success
  */
 int PEQ_render(PEQ_DATA *data);
 
 /**
- * \brief destroys application
- * \param data PEQ_DATA type pointer
- * \returns 0 success; -1 fail
+ * @brief destroys application
+ * @param data PEQ_DATA type pointer
+* @warning PEQ CORE FUNCTION
+ * @return 0 success; -1 fail
  */
 int PEQ_clean(PEQ_DATA *data);
 
 /**
- * \brief primary control loop
- * \par handles framerate control and application loop
- * \param data PEQ_DATA type pointer
- * \returns 0 success; -1 fail
+ * @brief primary control loop
+ * @par handles framerate control and application loop.
+ * any tasks added here will run each frame.
+ * @note primary used to call other looped functions such as
+ * render, update, etc.
+ * @warning PEQ CORE FUNCTION
+ * @param data PEQ_DATA type pointer
+ * @return 0 success; -1 fail
  */
 int PEQ_cycle(PEQ_DATA *data);
 
